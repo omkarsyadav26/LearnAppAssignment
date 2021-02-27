4cdea0231b204f7087d5a18bb3333b54699e9a14
omkar yadav <omkarsyadav26@gmail.com>
2021-02-27 07:57:42 +0000

    initial commit with basic post call validation

---
+++ express-app/index.js
@@ -0,0 +1,44 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const someData = [
+{
+    "startDate": "2016-01-27",
+    "endDate": "2018-02-01",
+    "minCount": "2701",
+    "maxCount": "3001",
+}
+];
+const pattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+const schema = Joi.object({
+    startDate: Joi.string().regex(pattern),
+    endDate: Joi.string().regex(pattern),
+    minCount: Joi.number()
+        .integer(),
+    maxCount: Joi.number()
+        .integer(),
+});
+
+app.post('/api/somedata', (req, res) => {
+  const schemaCheck = schema.validate(req.body);
+  if (schemaCheck.error){
+    res.status(400).send(schemaCheck.error);
+    return;
+  }
+  const somedata = {
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    minCount: req.body.minCount,
+    maxCount: req.body.maxCount
+  }
+  someData.push(somedata);
+  console.log(req.body);
+  res.send(someData);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
